fbc2cbe0a5814c128476f932bbbb7c6e6a01214b
rimina <annaliisa.mattila@gmail.com>
2022-01-13 12:51:27 +0200

    Jaa pyyntöjen käsittely routeihin ja kontrollereihin!

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,18 +1,17 @@
-
-
-export function checkAuthenticated() { (req, res, next) => {
-    if(req.isAuthenticated()) {
-        next();
-    }   
-};
+export function checkAuthenticated() {
+    (req, res, next) => {
+        if (req.isAuthenticated()) {
+            next();
+        }
+    };
 }
 
-
-export function checkAdmin() { (req, res, next) => {
-    if(req.user.role === "admin") {
-        next();
-    } else {
-        res.status(404).json({message: "You do not have admin status"});
-    }
-};
+export function checkAdmin() {
+    (req, res, next) => {
+        if (req.user.role === "admin") {
+            next();
+        } else {
+            res.status(404).json({ message: "You do not have admin status" });
+        }
+    };
 }
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,12 +1,11 @@
 import express from "express";
-//import { checkAuthenticated, checkAdmin } from "../controllers/auth-controllers.js";
 
-import { 
-    waresByUser, 
-    myPage, 
-    register, 
-    login, 
-    myWares, 
+import {
+    waresByUser,
+    myPage,
+    register,
+    login,
+    myWares,
     ownBids,
     bidsByUser,
     newBid,
@@ -14,29 +13,28 @@ import {
     deleteProduct,
     deleteUser,
     updateYourInfo,
-    updateYourProductInfo, 
+    updateYourProductInfo,
     updateUserProductInfo,
     changeUserRole,
     logout,
     deleteAnyUser,
     deleteAnyProduct,
-    deleteAnyBid } from "../controllers/users-controllers.js";
+    deleteAnyBid,
+} from "../controllers/users-controllers.js";
 
 const router = express.Router();
 
-
-function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated()) {
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
         next();
     }
-    
 }
 
-function checkAdmin( req, res, next) {
-    if(req.user.role === "admin") {
+function checkAdmin(req, res, next) {
+    if (req.user.role === "admin") {
         next();
     } else {
-        res.status(404).json({message: "You do not have admin status"});
+        res.status(404).json({ message: "You do not have admin status" });
     }
 }
 
@@ -44,7 +42,7 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.get("/myPage", checkAuthenticated, myPage);
-router.get("/waresByUser", waresByUser); 
+router.get("/waresByUser", waresByUser);
 router.get("/myWares", checkAuthenticated, myWares);
 router.get("/ownBids", checkAuthenticated, ownBids);
 router.get("/bidsByUser", bidsByUser);
@@ -59,12 +57,21 @@ router.delete("/deleteUser", checkAuthenticated, deleteUser);
 router.put("/updateYourInfo", checkAuthenticated, updateYourInfo);
 router.put("/updateYourProductInfo", checkAuthenticated, updateYourProductInfo);
 
-router.put("/updateUserProductInfo", checkAuthenticated, checkAdmin, updateUserProductInfo);
+router.put(
+    "/updateUserProductInfo",
+    checkAuthenticated,
+    checkAdmin,
+    updateUserProductInfo
+);
 router.put("/changeUserRole", checkAuthenticated, checkAdmin, changeUserRole);
 
 router.delete("/deleteAnyUser", checkAuthenticated, checkAdmin, deleteAnyUser);
-router.delete("/deleteAnyProduct", checkAuthenticated, checkAdmin, deleteAnyProduct);
+router.delete(
+    "/deleteAnyProduct",
+    checkAuthenticated,
+    checkAdmin,
+    deleteAnyProduct
+);
 router.delete("/deleteAnyBid", checkAuthenticated, checkAdmin, deleteAnyBid);
 
 export default router;
-
